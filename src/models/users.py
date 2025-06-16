76da5f04f39b3b74473f9cb99f349d6b6df25565
deshanj-eimsky <deshan.jayashanka@eimsky.com>
2025-06-16 22:19:24 +0530

    feat : add new attributes to user model

--- src/models/users.py
+++ src/models/users.py
@@ -1,11 +1,14 @@
-from sqlalchemy import Column, Integer, String, Enum as SQLEnum
+from sqlalchemy import Column, Integer, String, Enum as SQLEnum, ForeignKey
 from src.config.database import Base
 from enum import Enum
+
+from src.models.policeStationData import PoliceStation
 
 class UserTypeEnum(str, Enum):
     SYSTEM_USER = "system_user"
     ADMIN = "admin"
     NORMAL_USER = "normal_user"
+    POLICE_STATION_OFFICER = "police_station_officer"
 
 class User(Base):
     __tablename__ = "users"
@@ -20,3 +23,7 @@ class User(Base):
     nationalId = Column(String(20), unique=True, index=True)
     email = Column(String(100), unique=True, index=True)
     hashed_password = Column(String(255))
+    is_active = Column(Integer, default=1)
+    police_station_id = Column(Integer, nullable=True)
+
+    police_station = Column(Integer, ForeignKey('police_stations.id'), nullable=True)
